0d0292a22799cda6e7380498f0bc0b7af99621f7
Maram Khattab <maram.khattab@hotmail.com>
2020-10-22 10:16:10 +0200

    changed menu items description

--- src/app/menu/menu-data.service.ts
+++ src/app/menu/menu-data.service.ts
@@ -1,10 +1,10 @@
 import { MenuItem } from '../shared/menu-item.model';
 export class MenuDataService {
     menuItems: MenuItem[] = [new MenuItem("Pizza", "Mushrooms, Green peppers, Fresh Tomatoes, Onions, and Black Olives Loaded on Marinara Sauce and Topped with Fresh Mozzarella Cheese.", 150, 0, "https://pngimg.com/uploads/pizza/pizza_PNG44043.png"),
-    new MenuItem("Burger", "Cheese burger with mushroom and bacon.", 100, 0, "https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4159.png"),
-    new MenuItem("Grilled Chicken", "Half roasterie chicken.", 70, 0, "https://pngimg.com/uploads/fried_chicken/fried_chicken_PNG14091.png"),
-    new MenuItem("Steak", "300g of premium grassfed meat", 250, 0, "https://pngimg.com/uploads/steak/steak_PNG85.png"),
-    new MenuItem("Mozzarella sticks", "Deep fried mozzarella sticks with our special marinara sauce", 80, 0, "https://pngimg.com/uploads/soup/soup_PNG76.png")
+    new MenuItem("Burger", "Chargrilled Beef Patty Topped  with American Cheese, Mushrooms, Tomato and Lettuce.", 100, 0, "https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4159.png"),
+    new MenuItem("Grilled Chicken", "Rotisserie Cicken Served with a Side of French Fries .", 70, 0, "https://pngimg.com/uploads/fried_chicken/fried_chicken_PNG14091.png"),
+    new MenuItem("Steak", "300g of Premium Grass-fed Meat", 300, 0, "https://pngimg.com/uploads/steak/steak_PNG85.png"),
+    new MenuItem("Tomato soup", "Fresh Creamy Tomato Soup with Basil and Herbs.", 50, 0, "https://pngimg.com/uploads/soup/soup_PNG76.png")
     ]
     getMenuItems() {
         return this.menuItems;
